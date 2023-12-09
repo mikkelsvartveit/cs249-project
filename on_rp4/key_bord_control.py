aca460646c615efd48717223e2f4e361fad499ba
Thomas-Kragerud <thomas.kragerud@gmail.com>
2023-12-08 23:36:32 -0800

    working keyboard control from pi

---
+++ on_rp4/key_bord_control.py
@@ -0,0 +1,37 @@
+import serial
+import time
+import sys
+import tty
+import termios
+
+# pi4 TX GPIO 14
+# pi4 RX GPIO 15
+
+ser = serial.Serial('/dev/serial0', 9600)
+
+def send_command(cmd):
+    ser.write(cmd.encode())
+
+
+def get_char():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+        if ord(ch) == 3:  # ASCII Code for Ctrl-C
+            raise KeyboardInterrupt
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+try:
+    while True:
+        message = get_char()
+        if message in ['w', 'd', 's', 'a', 'f']:
+            print("Sending message:", message)
+            ser.write(message.encode())
+        elif message == 'q':
+            break
+except KeyboardInterrupt:
+    print("\nInterrupted by user")
