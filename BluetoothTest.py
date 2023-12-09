9a1699d5191a6686f2a5d3e957027275fd3691a4
Thomas Kragerud <thomas.kragerud@gmail.com>
2023-12-08 21:28:19 -0800

    small fix to bluetooth

--- BluetoothTest.py
+++ BluetoothTest.py
@@ -1,8 +1,9 @@
 import serial
 import keyboard
 
-serial_port = "/dev/tty.HC-05"
-baud_rate = 9600
+
+serial_port = "/dev/tty.HC-05" # name of the bluetooth device 
+baud_rate = 9600               # baud rate for communication 
 
 try:
     bluetooth_serial = serial.Serial(serial_port, baud_rate, timeout=1)
@@ -15,18 +16,19 @@ try:
 
         message = keyboard.read_key()
         if (message == 'w' or message == 'd' or message == 's' or message == 'a' or message == 'f'):
-            print("Sending message:", message)
+            #print("Sending message:", message)
             bluetooth_serial.write(message.encode())
         elif (message == 'q'):
             break
+    
 
         # Wait for response
-        # while bluetooth_serial.inWaiting() <= 0:
-        #     continue
+        #while bluetooth_serial.inWaiting() <= 0:
+        #    continue
 
         # # Receiving response
-        # incoming = bluetooth_serial.readline().decode()
-        # print("Received message:", incoming)
+        #incoming = bluetooth_serial.readline().decode()
+        #print(f"\nReceived message:", incoming)
 
 except serial.SerialException as e:
     print(f"Error connecting to {serial_port}: {e}")
